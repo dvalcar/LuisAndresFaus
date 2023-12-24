4ec80de0364c4e07baafe2b3daebce63826ac6b4
David Valcarcel Rodriguez <104252957+dvalcar@users.noreply.github.com>
2023-12-24 18:54:14 +0100

    commit inicial

---
+++ photogallery/photo31260/_vti_cnf/sldshow.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|19 Dec 2013 11:05:42 -0000
+vti_author:SR|VALKILLER\\Sara y
+vti_modifiedby:SR|NAS2\\David
+vti_nexttolasttimemodified:TW|19 Dec 2013 11:05:42 -0000
+vti_timecreated:TR|02 Oct 2005 10:07:53 -0000
+vti_lineageid:SR|{9D8535AC-7E29-4B62-99FA-5E93E94B04BD}
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|photogallery/photo31260/real.htm
+vti_cacheddtm:TX|03 Dec 2013 20:44:00 -0000
+vti_filesize:IR|4651
